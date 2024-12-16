b21312338a30d8e8104d73d86f3e76db21099c8d
SantiSaraceni <155652199+SantiSaraceni@users.noreply.github.com>
2024-12-16 11:50:20 -0300

    Update computes_norms_and_angles_of_vectors.py
    I added some code to test that the function is working properly.

--- computes_norms_and_angles_of_vectors.py
+++ computes_norms_and_angles_of_vectors.py
@@ -1,13 +1,27 @@
+import numpy as np
+from numpy import linalg as LA
+
 def calculate_norm_and_angles(ordered_array):
-  coeff_norms = LA.norm(ordered_array,axis = 1) #Calcula la norma cada vector, tomando como vector a cada una de las filas del array ordenado. Devuelve un array 1D con todas las normas.
-  angulos = np.array([])
-  for i in range (0,len(ordered_array)-1,1): #A partir de aca calcula los angulos
+  coeff_norms = LA.norm(ordered_array,axis = 1) #Computes each vector norm, taking each row of the ordered array as a vector. Returns a 1D array with all the norms
+  angles = np.array([]) 
+  for i in range (0,len(ordered_array)-1,1): #From here it computes the angles
     for j in range (i+1,len(ordered_array),1):
       if coeff_norms[j] < 10**-6 or coeff_norms[i] < 10**-6:
-        alfa = 0
-        angulos = np.append(angulos, alfa)
+        alpha = 0
+        angles = np.append(angles, alpha)
       else:
-        alfa = np.arccos(np.dot(ordered_array[i,:],ordered_array[j,:])/(coeff_norms[i]*coeff_norms[j]))
-        angulos = np.append(angulos, alfa)
-  norm_and_angles = np.concatenate((coeff_norms,angulos)) #Concatena las dos variables
-  return norm_and_angles #Devuelve un array 1D con las normas y los angulos.
+        alpha = np.arccos(np.dot(ordered_array[i,:],ordered_array[j,:])/(coeff_norms[i]*coeff_norms[j]))
+        angles = np.append(angles, alpha)
+  norm_and_angles = np.concatenate((coeff_norms,angles)) #Concatenates the two variables
+  return norm_and_angles #Returns a 1D array with the norms and the angles
+
+#TEST CODE TO CHECK THE NORMS AND ANGLES ARE CORRECT
+
+test_vectors = list()
+for i in range(0,3,1):
+  test_vector = list((0,0,0))
+  test_vector[i] = 1
+  test_vectors.append(test_vector)
+
+test_vectors = np.array(test_vectors).reshape((3,3))
+norms_and_angles_test = calculate_norm_and_angles(test_vectors)
