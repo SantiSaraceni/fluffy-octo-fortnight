3e5969095f06c0c8aca5de01deeca4a3ad8f67d3
SantiSaraceni <155652199+SantiSaraceni@users.noreply.github.com>
2024-12-28 21:23:43 -0300

    Update computes_norms_and_angles_of_vectors.py
    Contains a more detailed calculation of every term so to check whether its correct or not

--- computes_norms_and_angles_of_vectors.py
+++ computes_norms_and_angles_of_vectors.py
@@ -3,14 +3,18 @@ from numpy import linalg as LA
 
 def calculate_norm_and_angles(ordered_array):
   coeff_norms = LA.norm(ordered_array,axis = 1) #Computes each vector norm, taking each row of the ordered array as a vector. Returns a 1D array with all the norms
-  angles = np.array([]) 
+  angles = np.array([])
   for i in range (0,len(ordered_array)-1,1): #From here it computes the angles
     for j in range (i+1,len(ordered_array),1):
       if coeff_norms[j] < 10**-6 or coeff_norms[i] < 10**-6:
         alpha = 0
         angles = np.append(angles, alpha)
       else:
-        alpha = np.arccos(np.dot(ordered_array[i,:],ordered_array[j,:])/(coeff_norms[i]*coeff_norms[j]))
+        producto_escalar = np.dot(ordered_array[i,:],ordered_array[j,:])
+        denominador = coeff_norms[i]*coeff_norms[j]
+        cociente = producto_escalar/denominador
+        alpha = np.arccos(cociente)
+        print(producto_escalar, denominador, cociente, alpha)
         angles = np.append(angles, alpha)
   norm_and_angles = np.concatenate((coeff_norms,angles)) #Concatenates the two variables
   return norm_and_angles #Returns a 1D array with the norms and the angles
